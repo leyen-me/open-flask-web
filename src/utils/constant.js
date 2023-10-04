68a712bea4bf12644e15b04d37159d9f0bb57e3c
DIFFFFFFT <672228275@qq.com>
2023-10-04 14:49:26 +0800

    新增附件

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -6,9 +6,9 @@ export default {
     // API地址
     apiUrl: import.meta.env.VITE_APP_BASE_API,
     // 文件上传地址
-    uploadUrl: import.meta.env.VITE_APP_BASE_API + '/sys/file/upload?access_token=',
+    uploadUrl: import.meta.env.VITE_APP_BASE_API + '/sys/file/upload',
     // 导入用户excel文件
-    uploadUserExcelUrl: import.meta.env.VITE_APP_BASE_API + '/sys/user/import?access_token=',
+    uploadUserExcelUrl: import.meta.env.VITE_APP_BASE_API + '/sys/user/import',
     // 环境变量
     env: {
         MODE: import.meta.env.MODE,
