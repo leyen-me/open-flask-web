b7dc0ba1044dc41b9383ca8252d8b856b7bba31d
DIFFFFFFT <672228275@qq.com>
2023-10-10 17:33:06 +0800

    等待优化useCrud

---
+++ src/constants/modules/constantDict.js
@@ -0,0 +1,2 @@
+const constantDict = []
+export default constantDict
--- src/store/modules/appStore.js
+++ src/store/modules/appStore.js
@@ -1,12 +1,13 @@
 import {defineStore} from 'pinia'
 import {useDictTypeAllApi} from '@/api/sys/dict'
+import constantDict from "@/constants/modules/constantDict";
 
 const useAppStore = defineStore('appStore', {
     state: () => ({
         // sidebar 是否展开
         sidebarOpened: false,
         // 字典列表
-        dictList: [],
+        dictList: constantDict,
     }),
     actions: {
         setSidebarOpened() {
